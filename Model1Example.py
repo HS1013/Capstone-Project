de09180d3763737d1861b8f3cf44c2ee8c72a746
HMS <107879203+HS1013@users.noreply.github.com>
2025-05-02 14:04:28 -0400

    Update Model1Example.py

--- Model1Example.py
+++ Model1Example.py
@@ -1,5 +1,4 @@
 
-
 from transformers import pipeline
 
 summarizer = pipeline("summarization", model="Falconsai/text_summarization")
@@ -29,4 +28,3 @@ Hugging Face's story is one of transformation, collaboration, and empowerment. T
 
 print(summarizer(ARTICLE, max_length=425, min_length=30, do_sample=False))
 #>>> [{'summary_text': 'Hugging Face has emerged as a prominent and innovative force in NLP . From its inception to its role in democratizing AI, the company has left an indelible mark on the industry . The name "Hugging Face" was chosen to reflect the company\'s mission of making AI models more accessible and friendly to humans .'}]
-
